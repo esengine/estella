dff553e95e53a73d0146b023d2bee49396eddf46
esengine <359807859@qq.com>
2026-04-17 14:57:10 +0800

    refactor(ui): delete UIBuilder legacy declarative tree
    892-line declarative UI tree builder. Only self-referenced; no consumers
    across the repo. The ui2/ widgets provide factories (createButton,
    createSlider, etc.) that replace this.

--- sdk/src/ui/index.ts
+++ sdk/src/ui/index.ts
@@ -366,37 +366,6 @@ export {
     type ParsedPropertyPath,
 } from './propertyPath';
 
-// UI Builder (Factory Functions & Declarative Tree)
-export {
-    UI,
-    initUIBuilder,
-    type UIEntityDef,
-    type ButtonOptions,
-    type SliderOptions,
-    type ToggleOptions,
-    type ProgressBarOptions,
-    type ScrollViewOptions,
-    type TextInputOptions,
-    type DropdownOptions,
-    type LabelOptions,
-    type PanelOptions,
-    type FlexOptions,
-    type UINode,
-    type UIElementNode,
-    type UIButtonNode,
-    type UISliderNode,
-    type UIToggleNode,
-    type UITextInputNode,
-    type UIDropdownNode,
-    type UIProgressBarNode,
-    type UILabelNode,
-    type UIPanelNode,
-    type UIFlexRowNode,
-    type UIFlexColumnNode,
-    type UIScrollViewNode,
-} from './UIBuilder';
-
-
 // DataBinding
 export {
     DataBinding,
